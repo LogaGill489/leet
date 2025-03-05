6e509e9d12d62a9a75418f865842932b4ccde7b5
LogaGill489 <logagill489@ed.amdsb.ca>
2025-03-05 12:59:51 -0500

    Added a smarter solution to the cell problem that uses some simple math to remove the for loop an bring the function to a single line.

--- daily/countTotalCells.cpp
+++ daily/countTotalCells.cpp
@@ -12,6 +12,15 @@ class poorSln {
         }
 };
 
+class quickSln {
+    public:
+        //this solution uses the integral of 4n, which gives 2x^2 + c (1), with an increasing offset to the actual answers by 2n
+        //Thus we just add a -2n to the solution to get the correct result.
+        long long coloredCells(int n) {
+            return 2*pow(n, 2) - 2*n + 1;
+        }
+};
+
 int main() {
     int n = 3;
     poorSln sol;
